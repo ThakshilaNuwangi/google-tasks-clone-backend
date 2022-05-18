4f6f081a35046f589209565fc3259a5fca5ba32c
Thakshila Nuwangi <thakshilanuwangi123@gmail.com>
2022-05-18 19:51:50 +0530

    add http components to the project

---
+++ src/main/java/lk/ijse/dep8/tasks/util/HttpResponseErrorMsg.java
@@ -0,0 +1,82 @@
+package lk.ijse.dep8.tasks.util;
+
+public class HttpResponseErrorMsg {
+    private long timeStamp;
+    private int status;
+    private String error;
+    private String exception;
+    private String message;
+    private String path;
+
+    public HttpResponseErrorMsg() {
+    }
+
+    public HttpResponseErrorMsg(long timeStamp, int status, String error, String exception, String message, String path) {
+        this.timeStamp = timeStamp;
+        this.status = status;
+        this.error = error;
+        this.exception = exception;
+        this.message = message;
+        this.path = path;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getException() {
+        return System.getProperty("app.profile.active").equals("dev")? exception : null;
+    }
+
+    public void setException(String exception) {
+        this.exception = exception;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    @Override
+    public String toString() {
+        return "HttpResponseErrorMsg{" +
+                "timeStamp=" + timeStamp +
+                ", status=" + status +
+                ", error='" + error + '\'' +
+                ", exception='" + exception + '\'' +
+                ", message='" + message + '\'' +
+                ", path='" + path + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/lk/ijse/dep8/tasks/util/HttpServlet2.java
@@ -0,0 +1,49 @@
+package lk.ijse.dep8.tasks.util;
+
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class HttpServlet2 extends HttpServlet {
+
+    private Logger logger = Logger.getLogger(HttpServlet2.class.getName());
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            super.service(req, resp);
+        } catch (Throwable t) {
+            logger.logp(Level.SEVERE, t.getStackTrace()[0].getClassName(),
+                    t.getStackTrace()[0].getMethodName(), t.getMessage(), t);
+
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            t.printStackTrace(pw);
+
+            resp.setContentType("application/json");
+            HttpResponseErrorMsg errorMsg = null;
+            if (t instanceof ResponseStatusException) {
+                ResponseStatusException rse = (ResponseStatusException) t;
+                resp.setStatus(rse.getStatus());
+                errorMsg = new HttpResponseErrorMsg(new Date().getTime(), rse.getStatus() , "Internal server error", t.getLocalizedMessage(),
+                        sw.toString(), req.getRequestURI());
+            } else {
+
+            }
+
+
+            Jsonb jsonb = JsonbBuilder.create();
+            jsonb.toJson(errorMsg, resp.getWriter());
+        }
+    }
+}
