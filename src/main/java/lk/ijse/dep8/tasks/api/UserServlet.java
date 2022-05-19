7c9b90f84fad970df15b55a1cb436a209e7f6c87
Thakshila Nuwangi <thakshilanuwangi123@gmail.com>
2022-05-19 15:08:52 +0530

    save data to the database

--- src/main/java/lk/ijse/dep8/tasks/api/UserServlet.java
+++ src/main/java/lk/ijse/dep8/tasks/api/UserServlet.java
@@ -1,16 +1,25 @@
 package lk.ijse.dep8.tasks.api;
 
 import lk.ijse.dep8.tasks.util.HttpServlet2;
+import lk.ijse.dep8.tasks.util.ResponseStatusException;
 
 import javax.annotation.Resource;
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 import javax.sql.DataSource;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.*;
 import java.util.logging.Logger;
 
-@WebServlet(name = "UserServlet", value = "/users/*")
+@MultipartConfig(location = "/tmp", maxFileSize = 5 * 1024 * 1024)
+@WebServlet(name = "UserServlet", value = "/v1/users/*")
 public class UserServlet extends HttpServlet2 {
 
     private final Logger logger = Logger.getLogger(UserServlet.class.getName());
@@ -26,5 +35,51 @@ public class UserServlet extends HttpServlet2 {
             return;
         }
 
+        if (req.getPathInfo() != null && !req.getPathInfo().equals("/")) {
+            throw new ResponseStatusException(HttpServletResponse.SC_METHOD_NOT_ALLOWED, "Invalid end point for POST requests");
+        }
+
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+        Part picture = req.getPart("picture");
+
+        if (name == null || !name.matches("[A-Za-z ]+")) {
+            throw new ResponseStatusException(HttpServletResponse.SC_BAD_REQUEST, "Invalid name or name is empty");
+        } else if (email == null || !email.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")) {
+            throw new ResponseStatusException(HttpServletResponse.SC_BAD_REQUEST, "Invalid email or email is empty");
+        } else if (password == null || password.trim().isEmpty()) {
+            throw new ResponseStatusException(HttpServletResponse.SC_BAD_REQUEST, "Invalid password or password is empty");
+        } else if (picture != null && !picture.getContentType().startsWith("image")) {
+            throw new ResponseStatusException(HttpServletResponse.SC_BAD_REQUEST, "Invalid picture");
+        }
+
+        String appLocation = getServletContext().getRealPath("/");
+        Path path = Paths.get(appLocation, "uploads");
+        if (Files.notExists(path)) {
+            Files.createDirectory(path);
+        }
+
+        try (Connection connection = pool.getConnection()) {
+            PreparedStatement stm = connection.prepareStatement("INSERT INTO user(id, email, password, full_name) VALUES (uuid(),?,?,?)");
+            stm.setString(1, email);
+            stm.setString(2, password);
+            stm.setString(3, name);
+            if (stm.executeUpdate() != 1) {
+                throw new SQLException("Failed to register the user");
+            }
+
+            stm = connection.prepareStatement("SELECT * from user WHERE email=?");
+            stm.setString(1, email);
+            ResultSet rst = stm.executeQuery();
+            rst.next();
+            String uuid = rst.getString("id");
+
+            Path imagePath = path.resolve(uuid);
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
