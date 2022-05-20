6e361dba5f28075155c59e4a78817d4050115a4c
Thakshila Nuwangi <thakshilanuwangi123@gmail.com>
2022-05-20 11:50:19 +0530

    add some validations

--- src/main/java/lk/ijse/dep8/tasks/api/TaskListServlet.java
+++ src/main/java/lk/ijse/dep8/tasks/api/TaskListServlet.java
@@ -1,6 +1,7 @@
 package lk.ijse.dep8.tasks.api;
 
 import lk.ijse.dep8.tasks.util.HttpServlet2;
+import lk.ijse.dep8.tasks.util.ResponseStatusException;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -17,11 +18,22 @@ public class TaskListServlet extends HttpServlet2 {
             resp.sendError(HttpServletResponse.SC_NOT_FOUND);
         }
 
-        String pattern = "/[A-Fa-f0-9\\-]{36}/lists/?.*";
+        String pattern = "/users/[A-Fa-f0-9\\-]{36}/lists/?.*";
         if (pathInfo.matches(pattern)){
             super.service(req, resp);
         }else{
             resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getContentType() == null || !req.getContentType().startsWith("application/json")) {
+            throw new ResponseStatusException(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE, "Invalid content type or content type is empty");
+        }
+        String pattern = "/users/[A-Fa-f0-9\\-]{36}/lists/?";
+        if (req.getPathInfo().matches(pattern)) {
+            throw new ResponseStatusException(HttpServletResponse.SC_METHOD_NOT_ALLOWED, "Invalid end point for POST request");
         }
     }
 }
