41b3400ffa802e2027561822378aa690cc3a0d06
Thakshila Nuwangi <thakshilanuwangi123@gmail.com>
2022-05-20 14:47:50 +0530

    finalize doPost() method

--- src/main/java/lk/ijse/dep8/tasks/api/TaskListServlet.java
+++ src/main/java/lk/ijse/dep8/tasks/api/TaskListServlet.java
@@ -1,28 +1,41 @@
 package lk.ijse.dep8.tasks.api;
 
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import jakarta.json.bind.JsonbException;
+import lk.ijse.dep8.tasks.dto.TaskListDTO;
 import lk.ijse.dep8.tasks.util.HttpServlet2;
 import lk.ijse.dep8.tasks.util.ResponseStatusException;
 
+import javax.annotation.PostConstruct;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.*;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @WebServlet(name = "TaskListServlet")
 public class TaskListServlet extends HttpServlet2 {
 
-    @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String pathInfo = req.getPathInfo();
-        if (pathInfo == null){
-            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
-        }
+    private final Logger logger = Logger.getLogger(TaskListServlet.class.getName());
+    private AtomicReference<DataSource> pool;
 
-        String pattern = "/users/[A-Fa-f0-9\\-]{36}/lists/?.*";
-        if (pathInfo.matches(pattern)){
-            super.service(req, resp);
-        }else{
-            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+    @PostConstruct
+    public void init(){
+        try {
+            InitialContext ctx = new InitialContext();
+            DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/pool");
+            pool = new AtomicReference<>(ds);
+        } catch (NamingException e) {
+            logger.log(Level.SEVERE, "Failed to locate the JNDI pool", e);
         }
     }
 
@@ -31,9 +44,57 @@ public class TaskListServlet extends HttpServlet2 {
         if (req.getContentType() == null || !req.getContentType().startsWith("application/json")) {
             throw new ResponseStatusException(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE, "Invalid content type or content type is empty");
         }
-        String pattern = "/users/[A-Fa-f0-9\\-]{36}/lists/?";
-        if (req.getPathInfo().matches(pattern)) {
+
+        String pattern = "/([A-Fa-f0-9\\-]{36})/lists/?";
+        if (!req.getPathInfo().matches(pattern)) {
+            System.out.println(req.getPathInfo().matches(pattern));
             throw new ResponseStatusException(HttpServletResponse.SC_METHOD_NOT_ALLOWED, "Invalid end point for POST request");
         }
+        Matcher matcher = Pattern.compile(pattern).matcher(req.getPathInfo());
+        matcher.find();
+        String userId = matcher.group(1);
+
+        try (Connection connection = pool.get().getConnection()) {
+            PreparedStatement stm = connection.prepareStatement("SELECT * FROM user WHERE id=?");
+            stm.setString(1, userId);
+            ResultSet rst = stm.executeQuery();
+            if (!rst.next()) {
+                throw new ResponseStatusException(HttpServletResponse.SC_NOT_FOUND, "Invalid user ID");
+            }
+            Jsonb jsonb = JsonbBuilder.create();
+            TaskListDTO taskListDTO = jsonb.fromJson(req.getReader(), TaskListDTO.class);
+
+            if (taskListDTO.getTitle().trim().isEmpty()) {
+                throw new ResponseStatusException(HttpServletResponse.SC_BAD_REQUEST, "Invalid title or title is empty");
+            }
+
+            stm = connection.prepareStatement("INSERT INTO task_list (name, user_id) VALUES (?,?)", Statement.RETURN_GENERATED_KEYS);
+            stm.setString(1, taskListDTO.getTitle());
+            stm.setString(2, userId);
+
+            if (stm.executeUpdate()!=1) {
+                throw new SQLException("Failed to save the task list");
+            }
+
+            rst = stm.getGeneratedKeys();
+            rst.next();
+            taskListDTO.setId(rst.getInt(1));
+
+            resp.setContentType("application/json");
+            resp.setStatus(HttpServletResponse.SC_CREATED);
+            jsonb.toJson(taskListDTO,resp.getWriter());
+
+        } catch (SQLException | JsonbException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    private TaskListDTO getTaskListDTO(){
+
     }
 }
