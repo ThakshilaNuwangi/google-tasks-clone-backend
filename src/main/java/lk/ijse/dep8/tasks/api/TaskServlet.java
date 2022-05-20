b4a92ebebc946d7bf6f0025a58bb48232ee7ee9c
Thakshila Nuwangi <thakshilanuwangi123@gmail.com>
2022-05-20 17:08:24 +0530

    add task servlet

---
+++ src/main/java/lk/ijse/dep8/tasks/api/TaskServlet.java
@@ -0,0 +1,13 @@
+package lk.ijse.dep8.tasks.api;
+
+import lk.ijse.dep8.tasks.util.HttpServlet2;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "TaskServlet")
+public class TaskServlet extends HttpServlet2 {
+
+}
