7f17573362a0f450e90db358a5dacfb4f54fc97c
Thakshila Nuwangi <thakshilanuwangi123@gmail.com>
2022-05-25 00:42:33 +0530

    Exclude user registration from the SecurityFilter

--- src/main/java/lk/ijse/dep8/tasks/security/SecurityFilter.java
+++ src/main/java/lk/ijse/dep8/tasks/security/SecurityFilter.java
@@ -1,8 +1,30 @@
 package lk.ijse.dep8.tasks.security;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-@WebFilter
+@WebFilter(filterName = "SecurityFilter", urlPatterns = "/*")
 public class SecurityFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+        /*Excludes from security filter*/
+        String appContextPath = req.getContextPath();
+        if (req.getRequestURI().matches(appContextPath+"/v1/users/?") && req.getMethod().equals("POST")) {
+            chain.doFilter(req, res);
+            return;
+        }
+
+        String authorization = req.getHeader("Authorization");
+
+        if (authorization==null || !authorization.startsWith("Basic")) {
+            res.setStatus(403);
+        }
+    }
 }
