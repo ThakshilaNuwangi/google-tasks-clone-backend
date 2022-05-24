7e51664be6120404fae300a38228312d388fe3c3
Thakshila Nuwangi <thakshilanuwangi123@gmail.com>
2022-05-25 01:13:59 +0530

    finalize dispatcher servlet

--- src/main/java/lk/ijse/dep8/tasks/api/DispatcherServlet.java
+++ src/main/java/lk/ijse/dep8/tasks/api/DispatcherServlet.java
@@ -1,11 +1,15 @@
 package lk.ijse.dep8.tasks.api;
 
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
 import lk.ijse.dep8.tasks.security.SecurityContextHolder;
+import lk.ijse.dep8.tasks.util.HttpResponseErrorMsg;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -26,7 +30,11 @@ public class DispatcherServlet extends HttpServlet {
             if (matcher.find()){
                 String userId = matcher.group(1);
                 if (!userId.equals(SecurityContextHolder.getPrincipal().getId())){
+                    resp.setContentType("application/json");
                     resp.setStatus(403);
+                    Jsonb jsonb = JsonbBuilder.create();
+                    jsonb.toJson(new HttpResponseErrorMsg(new Date().getTime(), 403, null,
+                            "Permission denied", req.getRequestURI()), resp.getWriter());
                     return;
                 }
             }
@@ -43,6 +51,12 @@ public class DispatcherServlet extends HttpServlet {
                 getServletContext().getNamedDispatcher("TaskListServlet").forward(req, resp);
             }else if (req.getPathInfo().matches("/[A-Fa-f0-9\\-]{36}/lists/\\d+/tasks(/\\d+)?/?")) {
                 getServletContext().getNamedDispatcher("TaskServlet").forward(req, resp);
+            } else {
+                resp.setContentType("application/json");
+                resp.setStatus(404);
+                Jsonb jsonb = JsonbBuilder.create();
+                jsonb.toJson(new HttpResponseErrorMsg(new Date().getTime(), 404, null,
+                        "Invalid location", req.getRequestURI()), resp.getWriter());
             }
         }
 
