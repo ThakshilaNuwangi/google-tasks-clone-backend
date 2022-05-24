272e60f02740f4527e3159acae0e6c00bd491fb8
Thakshila Nuwangi <thakshilanuwangi123@gmail.com>
2022-05-25 00:34:26 +0530

    add security filter

---
+++ src/main/java/lk/ijse/dep8/tasks/security/SecurityFilter.java
@@ -0,0 +1,8 @@
+package lk.ijse.dep8.tasks.security;
+
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+
+@WebFilter
+public class SecurityFilter extends HttpFilter {
+}
