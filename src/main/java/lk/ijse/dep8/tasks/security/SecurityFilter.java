98a6ad78fc2e5fba95bd462bf8e098fb5d92fdd5
Thakshila Nuwangi <thakshilanuwangi123@gmail.com>
2022-05-25 00:55:37 +0530

    implement firewall logic

--- src/main/java/lk/ijse/dep8/tasks/security/SecurityFilter.java
+++ src/main/java/lk/ijse/dep8/tasks/security/SecurityFilter.java
@@ -1,15 +1,27 @@
 package lk.ijse.dep8.tasks.security;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
+import javax.annotation.Resource;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Base64;
 
 @WebFilter(filterName = "SecurityFilter", urlPatterns = "/*")
 public class SecurityFilter extends HttpFilter {
+
+    @Resource(name = "java:comp/env/jdbc/pool")
+    private volatile DataSource pool;
 
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
@@ -24,7 +36,34 @@ public class SecurityFilter extends HttpFilter {
         String authorization = req.getHeader("Authorization");
 
         if (authorization==null || !authorization.startsWith("Basic")) {
-            res.setStatus(403);
+            res.setStatus(401);
+            return;
+        }
+
+        String base64Credentials = authorization.replaceFirst("Basic ", "");
+        byte[] decodedByteArray = Base64.getDecoder().decode(base64Credentials);
+        String userCredentials = new String(decodedByteArray);
+
+        String[] split = userCredentials.split(":", 2);
+        String username = split[0];
+        String password = split[1];
+
+        try (Connection connection = pool.getConnection()) {
+            PreparedStatement stm = connection.prepareStatement("SELECT * FROM user WHERE email=?");
+            stm.setString(1, username);
+            ResultSet rst = stm.executeQuery();
+
+            if (!rst.next()) {
+                res.setStatus(401);
+                return;
+            }
+
+            if (!DigestUtils.sha256Hex(password).equals(rst.getString("password"))) {
+                res.setStatus(401);
+                return;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
