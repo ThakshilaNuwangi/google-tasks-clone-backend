e0abc583980410f249f42d35bb041d591b02a038
Thakshila Nuwangi <thakshilanuwangi123@gmail.com>
2022-06-08 11:52:54 +0530

    revamp the task and taskList entity with lombok annotations

--- src/main/java/lk/ijse/dep8/tasks/dao/TaskDAO.java
+++ src/main/java/lk/ijse/dep8/tasks/dao/TaskDAO.java
@@ -1,4 +1,21 @@
 package lk.ijse.dep8.tasks.dao;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TaskDAO {
+    private int id;
+    private String title;
+    private String details;
+    private int position;
+    private Status status;
+    private int taskListId;
+
+    public enum Status{
+        completed, needsAction
+    }
 }
--- src/main/java/lk/ijse/dep8/tasks/dao/TaskListDAO.java
+++ src/main/java/lk/ijse/dep8/tasks/dao/TaskListDAO.java
@@ -1,4 +1,14 @@
 package lk.ijse.dep8.tasks.dao;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TaskListDAO {
+    private int id;
+    private String name;
+    private String userId;
 }
